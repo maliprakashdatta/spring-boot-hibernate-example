23067260f7b76b43dcc6ddbb33bc6657e4772266
maliprakash092@gmail.com <maliprakash092@gmail.com>
2024-09-15 12:18:40 +0530

    COMMITTED

---
+++ src/main/java/com/swsa/SpringBootHibernateExampleApplication.java
@@ -0,0 +1,13 @@
+package com.swsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHibernateExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHibernateExampleApplication.class, args);
+	}
+
+}
